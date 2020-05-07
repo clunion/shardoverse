4735bb9b37e71a658e79efa49974c94c335dede1
clunion <Christian.Lunau@gmx.de>
2020-05-07 21:52:45 +0200

    fixed scripts (batch files) for AppVeyor stages, 2

--- ci/appveyor/x86_64-pc-windows-gnu_10_install.bat
+++ ci/appveyor/x86_64-pc-windows-gnu_10_install.bat
@@ -1,43 +1,41 @@
-  @echo off
+cd %APPVEYOR_BUILD_FOLDER%
 
-  cd %APPVEYOR_BUILD_FOLDER%
+set DIRCMD=/O:GN
+dir
 
-  echo Compiler: %COMPILER%
-  echo Architecture: %MSYS2_ARCH%
-  echo Platform: %PLATFORM%
-  echo MSYS2 directory: %MSYS2_DIR%
-  echo MSYS2 system: %MSYSTEM%
-  echo Bits: %BIT%
+echo Compiler: %COMPILER%
+echo Architecture: %MSYS2_ARCH%
+echo Platform: %PLATFORM%
+echo MSYS2 directory: %MSYS2_DIR%
+echo MSYS2 system: %MSYSTEM%
+echo Bits: %BIT%
 
-  REM Create a writeable TMPDIR
-  mkdir %APPVEYOR_BUILD_FOLDER%\tmp
-  set TMPDIR=%APPVEYOR_BUILD_FOLDER%\tmp
+REM Create a writeable TMPDIR
+mkdir %APPVEYOR_BUILD_FOLDER%\tmp
+set TMPDIR=%APPVEYOR_BUILD_FOLDER%\tmp
 
-  IF %COMPILER%==msys2 (
-    @echo on
-    SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
-    bash -lc "pacman -S --needed --noconfirm pacman-mirrors"
-    bash -lc "pacman -S --needed --noconfirm git"
+SET PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%
 
-    REM ### Updating the Msys2- and MinGW64-Packages and -Repositories:
-    bash -lc "pacman -Syu --noconfirm"
+bash -lc "pacman -S --needed --noconfirm pacman-mirrors"
+bash -lc "pacman -S --needed --noconfirm git"
 
-    REM ### Install the development toolchain:
-    bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-toolchain"
+REM ### Updating the Msys2- and MinGW64-Packages and -Repositories:
+bash -lc "pacman -Syu --noconfirm"
 
-    REM ### Add several necessary development tools:
-    bash -lc "pacman -S --needed --noconfirm base-devel"
-    bash -lc "pacman -S --needed --noconfirm msys2-devel"
+REM ### Install the development toolchain:
+bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-toolchain"
 
-    REM ### Installing the SDL2-Library:
-    REM To check which SDL2-Packets are available for Mingw64:
+REM ### Add several necessary development tools:
+bash -lc "pacman -S --needed --noconfirm base-devel"
+bash -lc "pacman -S --needed --noconfirm msys2-devel"
 
-    bash -lc "pacman -Ss --needed --noconfirm mingw-w64-x86_64-SDL2
+REM ### Installing the SDL2-Library:
+REM To check which SDL2-Packets are available for Mingw64:
 
-    REM #### Then at least install the following:
-    bash -lc "pacman -S --needed --noconfirm  mingw-w64-x86_64-SDL2_image"
-    bash -lc "pacman -S --needed --noconfirm  mingw-w64-x86_64-SDL2_ttf"
-    bash -lc "pacman -S --needed --noconfirm  mingw-w64-x86_64-SDL2_mixer"
-    bash -lc "pacman -S --needed --noconfirm  mingw-w64-x86_64-SDL2_gfx"
+bash -lc "pacman -Ss --needed --noconfirm mingw-w64-x86_64-SDL2
 
-  )
+REM #### Then at least install the following:
+bash -lc "pacman -S --needed --noconfirm  mingw-w64-x86_64-SDL2_image"
+bash -lc "pacman -S --needed --noconfirm  mingw-w64-x86_64-SDL2_ttf"
+bash -lc "pacman -S --needed --noconfirm  mingw-w64-x86_64-SDL2_mixer"
+bash -lc "pacman -S --needed --noconfirm  mingw-w64-x86_64-SDL2_gfx"
