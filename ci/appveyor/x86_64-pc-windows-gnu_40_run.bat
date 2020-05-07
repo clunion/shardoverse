7c398a3c6ee1228642b59e5f39ad04c2fa76483f
clunion <Christian.Lunau@gmx.de>
2020-05-08 00:11:13 +0200

    added ci/appveyor/x86_64-pc-windows-gnu_40_run.bat

---
+++ ci/appveyor/x86_64-pc-windows-gnu_40_run.bat
@@ -0,0 +1,3 @@
+@echo ### since shardoverse is a game with a gui, which is not accessible in the CI-pipline, it should not be run here!
+REM cargo run  --target %TARGET%
+REM cargo run  --target %TARGET% --release
