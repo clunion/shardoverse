cfb3250878d321ba419036a74d03b7863f8c8c76
clunion <Christian.Lunau@gmx.de>
2020-05-09 22:08:35 +0200

    TravisCI, simplified scripts

--- ci/travisci/x86_64-unknown-linux-gnu_10_install.sh
+++ ci/travisci/x86_64-unknown-linux-gnu_10_install.sh
@@ -1,55 +1,18 @@
 set -ex
 
-main() {
-    echo "--------------------------------------------------------"
-    uname -a
-    
-    echo "--------------------------------------------------------"
-    sudo apt-cache policy
-    
-    echo "--------------------------------------------------------"
-    sudo apt-get update
-    
-    echo "--------------------------------------------------------"
-    sudo apt list --installed
+echo "--------------------------------------------------------"
+uname -a
 
-    echo "--------------------------------------------------------"
-    sudo apt-get install libsdl2-dev
+echo "--------------------------------------------------------"
+sudo apt-cache policy
 
+echo "--------------------------------------------------------"
+sudo apt-get update
 
-    # Builds for iOS are done on OSX, but require the specific target to be
-    # installed.
-    case $TARGET in
-        aarch64-apple-ios)
-            rustup target install aarch64-apple-ios
-            ;;
-        armv7-apple-ios)
-            rustup target install armv7-apple-ios
-            ;;
-        armv7s-apple-ios)
-            rustup target install armv7s-apple-ios
-            ;;
-        i386-apple-ios)
-            rustup target install i386-apple-ios
-            ;;
-        x86_64-apple-ios)
-            rustup target install x86_64-apple-ios
-            ;;
-    esac
+echo "--------------------------------------------------------"
+sudo apt list --installed
+
+echo "--------------------------------------------------------"
+sudo apt-get install libsdl2-dev
 
 
-    # This fetches latest stable release
-    # local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
-    #                    | cut -d/ -f3 \
-    #                    | grep -E '^v[0.1.0-9.]+$' \
-    #                    | $sort --version-sort \
-    #                    | tail -n1)
-    # curl -LSfs https://japaric.github.io/trust/install.sh | \
-    #     sh -s -- \
-    #        --force \
-    #        --git japaric/cross \
-    #        --tag $tag \
-    #        --target $target
-}
-
-main
--- ci/travisci/x86_64-unknown-linux-gnu_20_script.sh
+++ ci/travisci/x86_64-unknown-linux-gnu_20_script.sh
@@ -2,23 +2,9 @@
 
 set -ex
 
-# TODO This is the "test phase", tweak it as you see fit
-main() {
-    cross build --target $TARGET
-    cross build --target $TARGET --release
+cargo build
+cargo build --release
 
-    if [ ! -z $DISABLE_TESTS ]; then
-        return
-    fi
+cargo test
+cargo test --release
 
-    cross test --target $TARGET
-    cross test --target $TARGET --release
-
-    # cross run --target $TARGET
-    # cross run --target $TARGET --release
-}
-
-# we don't run the "test phase" when doing deploys
-if [ -z $TRAVIS_TAG ]; then
-    main
-fi
