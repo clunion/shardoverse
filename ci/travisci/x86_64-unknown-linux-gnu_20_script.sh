0fd09de84f8e8827401206ccbbcea70938aef6e3
clunion <Christian.Lunau@gmx.de>
2020-05-11 19:37:15 +0200

    Hm, found the SDL2-Libs, now setting the LIBRARY_PATH in the build&run-script

--- ci/travisci/x86_64-unknown-linux-gnu_20_script.sh
+++ ci/travisci/x86_64-unknown-linux-gnu_20_script.sh
@@ -2,6 +2,11 @@ echo in Script: $0
 
 set -ex
 
+# searching for the SDL2-Libs, to get the correct path:
+find /usr/local -name libSDL2-2.0.so.0
+ls -al /usr/local/lib/
+export LIBRARY_PATH=/usr/local/lib/
+
 cargo build
 cargo build --release
 
