5c5855be72fd271fe10c61b7ce8091f418ca9411
clunion <Christian.Lunau@gmx.de>
2020-05-11 21:37:28 +0200

    TravisCI, checking Shell-Environment

--- ci/travisci/x86_64-unknown-linux-gnu_20_script.sh
+++ ci/travisci/x86_64-unknown-linux-gnu_20_script.sh
@@ -5,7 +5,10 @@ set -ex
 # searching for the SDL2-Libs, to get the correct path:
 find /usr/local -name libSDL2-2.0.so.0
 ls -al /usr/local/lib/
-export LIBRARY_PATH=/usr/local/lib/
+env
+export LIBRARY_PATH=/usr/local/lib/:$LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
+env
 
 cargo build
 cargo build --release
