bc7c33a61087172a8e4c612b7081e92036627f74
clunion <Christian.Lunau@gmx.de>
2020-07-04 22:09:55 +0200

    test git_commit-push.sh

--- bin/git_commit-push.sh
+++ bin/git_commit-push.sh
@@ -1,15 +1,23 @@
-### shortcut for Rust's cargo unit-test of the target:
+### script to not forget to test and doc before a push to Git repository:
 
+#------------------------------------- 
 #cargo fmt <-- not yet, preferred format is still unstable
+#if ! cargo fmt; then
+#    echo cargo fmt returned error: $?
+#    exit
+#else
+#    echo format OK
+#fi 
 
+#------------------------------------- 
 if ! cargo test; then
     echo cargo test returned error: $?
     exit
 else
-    echo test OK
+    echo tests OK
 fi 
 
-
+#------------------------------------- 
 if ! cargo doc; then
     echo cargo doc returned error: $?
     exit
@@ -17,19 +25,19 @@ else
     echo docs OK
 fi 
 
-
+#------------------------------------- 
 if ! git commit -a; then
     echo git commit -a returned error: $?
     exit
 else
-    echo git commit: OK
+    echo commited OK
 fi 
 
-# 
+#------------------------------------- 
 if ! git push; then
     echo git push returned error: $?
     exit
 else
-    echo docs OK
+    echo pushed OK
 fi 
 
